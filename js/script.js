bb446ca7abab1a7a0c0b9527ac45d6f9c9d5113b
cmstufail <cmstufail@gmail.com>
2025-03-03 16:46:39 +0600

    added poppins fonts and updated al requirements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// dynamic page color array
+
 const colors = [
   "#808080",
   "#4682B4",
@@ -8,11 +10,16 @@ const colors = [
 ];
 let color = 0;
 
+// dynamic date
+
 const dayWeek = document.getElementById("day-week");
 const dayMonth = document.getElementById("date");
 const date = new Date();
 
+// weeks name array
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// months name array
 const monthOfYear = [
   "Jan",
   "Feb",
@@ -34,15 +41,17 @@ const month = monthOfYear[date.getMonth()];
 const dayOfMonth = date.getDate();
 const year = date.getFullYear();
 
-dayWeek.textContent = dayOfWeek + ",";
-dayMonth.textContent = `${dayOfMonth} ${month} ${year}`;
+dayWeek.textContent = dayOfWeek;
+dayMonth.textContent = `${month} ${dayOfMonth} ${year}`;
 
+// blog page link
 document
   .getElementById("enter-blog-page")
   .addEventListener("click", function () {
     window.location.href = "blog.html";
   });
 
+// dynamic page background change
 document
   .getElementById("change-bg-color")
   .addEventListener("click", function () {
@@ -50,60 +59,59 @@ document
     color = (color + 1) % colors.length;
   });
 
-
+// initial value
 let incrementCount = 23;
 let decrementCount = 6;
 
-const cardBtns = document.getElementsByClassName('card-btn');
-const clearHistory = document.getElementById('remove-history');
-const history = document.getElementById('history');
-const btnEnds = document.getElementById('btn-end');
-const headingTexts = document.querySelectorAll('.heading-text');
+const cardBtns = document.getElementsByClassName("card-btn");
+const clearHistory = document.getElementById("remove-history");
+const history = document.getElementById("history");
+const btnEnds = document.getElementById("btn-end");
+const headingTexts = document.querySelectorAll(".heading-text");
 
-for(let i = 0; i < cardBtns.length; i++){
+for (let i = 0; i < cardBtns.length; i++) {
   const cardBtn = cardBtns[i];
 
-  
+  cardBtn.addEventListener("click", function () {
+    incrementCount++;
 
-  cardBtn.addEventListener('click', function(){
-   
-    incrementCount ++;
-
-    if(decrementCount > 0){
-      decrementCount --;
+    if (decrementCount > 0) {
+      decrementCount--;
     }
-    document.getElementById('incrementDisplay').textContent = incrementCount;
-    document.getElementById('decrementDisplay').textContent = decrementCount;
+    document.getElementById("incrementDisplay").textContent = incrementCount;
+    document.getElementById("decrementDisplay").textContent = decrementCount;
 
-     alert("Board updated Successfully");
-     cardBtn.style.background = "#e5e7eb";
-     cardBtn.style.color = "#808080";
+    alert("Board updated Successfully");
+    cardBtn.style.background = "#e5e7eb";
+    cardBtn.style.color = "#808080";
 
-    const headingText = headingTexts[i] ? headingTexts[i].textContent : 'Task';
+    // cards heading text
+    const headingText = headingTexts[i] ? headingTexts[i].textContent : "Task";
 
+    // time
     const time = new Date();
     const timeString = time.toLocaleTimeString();
 
-    const div = document.createElement('div');
-    div.style.background = '#f4f7ff';
-    div.style.borderRadius = '10px';
-    div.style.padding = '8px';
-    div.style.marginBottom = '8px';
+    const div = document.createElement("div");
+    div.style.background = "#f4f7ff";
+    div.style.borderRadius = "10px";
+    div.style.padding = "8px";
+    div.style.marginBottom = "8px";
 
+    // for button click time show
     div.innerHTML = `
     <span>You have completed the task ${headingText} at ${timeString} </span>    
-    `
+    `;
     history.appendChild(div);
-  })
+  });
 }
-
-clearHistory.addEventListener('click', function(){
-  while(history.firstChild){
+// remove clear history
+clearHistory.addEventListener("click", function () {
+  while (history.firstChild) {
     history.removeChild(history.firstChild);
   }
-})
-
-document.getElementById('btn-end').addEventListener('click', function(){
-  alert('Congrates!!! You have completed all the current task')
-})
-
+});
+// last card button
+document.getElementById("btn-end").addEventListener("click", function () {
+  alert("Congrates!!! You have completed all the current task");
+});
